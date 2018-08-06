e7265d703e64457cd9ab938bfceedbade327aace
Daniel Ehrhardt <daniel.ehrhardt@bussmann-it.de>
2018-08-06 13:06:54 +0200

    Fixed #3

--- www/extended-device-information.js
+++ www/extended-device-information.js
@@ -21,24 +21,23 @@ function ExtendedDevice() {
 
     var me = this;
 
-    channel.onCordovaReady.subscribe(function () {
-        me.getInfo(function (info) {
-            //ignoring info.cordova returning from native, we should use value from cordova.version defined in cordova.js
-            //TODO: CB-5105 native implementations should not return info.cordova
-            var buildLabel = cordova.version;
-            console.log('Device Data', info);
+    if(cordova.platformId === 'android') {
+        channel.onCordovaReady.subscribe(function () {
+            me.getInfo(function (info) {
+                console.log('Device Data', info);
 
-            me.memory = info.memory || 'unknown';
-            me.cpumhz = info.cpumhz || 'unknown';
-            me.totalstorage = info.totalstorage || 'unknown';
-            me.freestorage = info.freestorage || 'unknown';
-            
-            channel.onCordovaReady.fire();
-        }, function (e) {
-            me.available = false;
-            utils.alert("[ERROR] Error initializing Cordova: " + e);
+                me.memory = info.memory || 'unknown';
+                me.cpumhz = info.cpumhz || 'unknown';
+                me.totalstorage = info.totalstorage || 'unknown';
+                me.freestorage = info.freestorage || 'unknown';
+                
+                channel.onCordovaReady.fire();
+            }, function (e) {
+                me.available = false;
+                utils.alert("[ERROR] Error initializing Cordova: " + e);
+            });
         });
-    });
+    }
 }
 
 /**
